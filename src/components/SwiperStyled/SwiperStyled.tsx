ee6a4a163378fe915335364dd7d4dfc5f888f62d
SVA2021 <sofronov.vit@gmail.com>
2022-10-06 18:42:16 +0400

    add mobile version

--- src/components/SwiperStyled/SwiperStyled.tsx
+++ src/components/SwiperStyled/SwiperStyled.tsx
@@ -1,18 +1,32 @@
 import {SwiperStyledProps} from "./SwiperStyled.types"
 import {Swiper, SwiperSlide} from "swiper/react";
-import {Navigation} from "swiper";
+import {Navigation, Pagination} from "swiper";
 import "swiper/scss";
 import "swiper/scss/navigation";
+import "swiper/css/pagination";
 import {FactCard} from "../FactCard";
 import './SwiperStyled.scss'
+import useWindowSize from "../../app/useWindowSize";
+import {useEffect, useState} from "react";
 
 export const SwiperStyled = ({items}: SwiperStyledProps) => {
+
+    const [deviceType, setDeviceType] = useState<'desktop' | 'mobile'>('desktop')
+    const realWidth = useWindowSize()
+
+    useEffect(() => {
+        if (!realWidth.width) return
+        if (realWidth.width < 1440 && deviceType !== 'mobile') setDeviceType('mobile')
+        if (realWidth.width >= 1440 && deviceType !== 'desktop') setDeviceType('desktop')
+    }, [realWidth.width, deviceType,])
+
     return (
         <Swiper
             navigation={true}
-            modules={[Navigation]}
+            pagination={deviceType === 'mobile'}
+            modules={[Navigation, Pagination]}
             slidesPerView={"auto"}
-            spaceBetween={80}
+            spaceBetween={deviceType === 'desktop' ? 80 : 25}
             className='mySwiper'>
             {items.map((item) =>
                 <SwiperSlide className='mySwiper__slide' key={item.year} >
